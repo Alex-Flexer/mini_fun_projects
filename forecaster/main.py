1b2e225dad18bbea4a5580bcf47e3b926b944d88
Alex-Flexer <sasha.flexer@gmail.com>
2025-05-14 15:06:10 +0300

    move yandex cookies to env file

--- forecaster/main.py
+++ forecaster/main.py
@@ -1,11 +1,20 @@
 from requests import get
+import asyncio
+from dotenv import dotenv_values
+import re
+
 from bs4 import BeautifulSoup
 from googletrans import Translator
 from transliterate import translit
-import asyncio
 
 
 translator = Translator()
+
+def parse_cookies(cookie_string: str) -> dict:
+    return {match.group(1): match.group(2) for match in (re.match(r'^([^=]+)=(.*)$', pair) for pair in cookie_string.split('; ')) if match}
+
+config = dotenv_values(".env")
+YANDEX_COOKIES = parse_cookies(config["COOKIES"])
 
 
 async def translator_translate(word):
@@ -14,8 +23,7 @@ async def translator_translate(word):
 
 
 def get_temperature(city: str):
-    headers = {"Cookie": 'yw_allergy_promo=1; _yasc=C/lSJLsx+LcIFqJEVJ5alUTH0LzCggmYJPuJnUO46jIusznQHntlJ8EOl8v7nLRSYYmAlQ8=; i=X0wHJMzYf0z2cQ8IWvoE0YLjpL6PJbVmy6fwOV0YEihHD2JYSS9tyTQWKE8Kq/oVyeZmqw9XM+Mcnng/aS/+Q0Wc0Rg=; yandexuid=4308001361737620354; yashr=9853474291737620354; bh=YIrR2cAGahLcyumIDvKso64E5cjwjgOUtgI=; is_gdpr=0; is_gdpr_b=CKKpXBD6twIoAg==; receive-cookie-deprecation=1; yuidss=4308001361737620354; yp=2058016242.multib.1#2058016310.udn.cDpzYXNoYWZsZXhzZXI%3D; yandex_login=sashaflexser; maps_routes_travel_mode=masstransit; Session_id=3:1745780859.5.0.1742656310235:vOmMsg:6697.1.2:1|973718500.-1.2.3:1742656310|3:10306641.448704.be7Wt_j_R7vFM8qsDExnrwhh-AI; sessar=1.1201.CiBZn0af6EvXv22ez8a5SV0EwrIxz7LOkrExTuj0sUJ9jA.F_DfrOD2TzV82SS4oA1VDJO24d2_fXjNBJfdQrDNjVc; sessionid2=3:1745780859.5.0.1742656310235:vOmMsg:6697.1.2:1|973718500.-1.2.3:1742656310|3:10306641.448704.fakesign0000000000000000000; skid=6127090731743349570; KIykI=1; coockoos=6'}
-    resp = get(f"https://yandex.ru/pogoda/ru/{city}", headers=headers)
+    resp = get(f"https://yandex.ru/pogoda/ru/{city}", cookies=YANDEX_COOKIES)
     res = None
     if resp.ok:
         bs = BeautifulSoup(resp.text, "html.parser")
